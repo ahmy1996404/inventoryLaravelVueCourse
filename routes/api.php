0688eb6d4199a582b77b9e9bf2b8e121a7f2c2ab
ahmy1996404 <a.hamodys@yahoo.com>
2022-03-27 12:29:52 +0200

    Project JWT Implement

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,18 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::group([
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout'] );
+    Route::post('refresh', [AuthController::class, 'refresh'] );
+    Route::post('me', [AuthController::class, 'me'] );
+
 });
